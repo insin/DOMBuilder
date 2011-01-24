d645a7a88f00c9bbbfbc059617211eded23a4a29
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2011-01-24 20:48:05 +0000

    Master list of tagnames now contains every element in the HTML 4.01 Strict specification
    git-svn-id: http://dombuilder.googlecode.com/svn/trunk@29 fee621d3-994e-0410-a6b3-dd72d6a7cdab

--- DOMBuilder.js
+++ DOMBuilder.js
@@ -350,15 +350,16 @@ var DOMBuilder = (function(document)
         apply: function(context)
         {
             context = context || {};
-            var tagNames = ["a", "abbr", "acronym", "address", "bdo",
-                "blockquote", "br", "button", "caption", "cite", "code", "col",
-                "colgroup", "dd", "del", "dfn", "div", "dl", "dt", "em",
-                "fieldset", "form", "h1", "h2", "h3", "h4", "h5", "h6", "hr",
-                "img", "input", "ins", "kbd", "label", "legend", "li", "link",
-                "object", "ol", "optgroup", "option", "p", "param", "pre",
-                "samp", "script", "select", "span", "strong", "style", "table",
-                "tbody", "td", "textarea", "tfoot", "th", "thead", "tr", "ul",
-                "var"];
+            var tagNames = ["a", "abbr", "acronym", "address", "area", "b", "bdo",
+                "big", "blockquote", "body", "br", "button", "caption", "cite",
+                "code", "col", "colgroup", "dd", "del", "dfn", "div", "dl", "dt",
+                "em", "fieldset", "form", "h1", "h2", "h3", "h4", "h5", "h6", "hr",
+                "head", "html", "i", "img", "input", "ins", "kbd", "label",
+                "legend", "li", "link", "map", "meta", "noscript"/*:)*/, "object",
+                "ol", "optgroup", "option", "p", "param", "pre", "q", "samp",
+                "script", "select", "small", "span", "strong", "style", "sub",
+                "sup", "table", "tbody", "td", "textarea", "tfoot", "th", "thead",
+                "title", "tr", "tt", "ul", "var"];
 
             for (var i = 0, tagName; tagName = tagNames[i]; i++)
             {
