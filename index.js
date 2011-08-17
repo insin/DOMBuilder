38a11195fa32a86fb5186651ee78e3ca59cf1089
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2011-08-18 00:38:41 +0100

    Added template mode to index.js and bumped package version

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var DOMBuilder = require('./lib/DOMBuilder');
 
-// Modes: html [default]
+// Modes: html [default], template
 require('./lib/DOMBuilder.html');
+require('./lib/DOMBuilder.template');
 
 module.exports = DOMBuilder;
